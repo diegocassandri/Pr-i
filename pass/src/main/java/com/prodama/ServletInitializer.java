c7e6fbcba4dbf23d2922918dda18a31697d403fa
diegocassandri <diego_aox886@hotmail.com>
2017-03-03 17:57:08 -0300

    dev

--- pass/src/main/java/com/prodama/ServletInitializer.java
+++ pass/src/main/java/com/prodama/ServletInitializer.java
@@ -1,28 +1,15 @@
 package com.prodama;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	
-	public static void main( String[] args ) throws Exception {
-		SpringApplication.run( PassApplication.class, args );
-	}
-	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {		
-		System.out.println("Initializing the WebXml!");		
-        return application.sources(applicationClass);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PassApplication.class);
     }
 
-    private static Class<PassApplication> applicationClass = PassApplication.class;
-	
+ 
+
 }
