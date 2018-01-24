9d0e60f16d6b803576825782464946b4bd288469
diegocassandri <diego_aox886@hotmail.com>
2018-01-24 17:39:30 -0200

    Alterações

--- pass/src/main/java/com/prodama/model/SimNao.java
+++
@@ -1,6 +0,0 @@
-package com.prodama.model;
-
-public enum SimNao {
-	  SIM,
-	  NÃO;
-	}
--- pass/src/main/java/com/prodama/model/TipoBase.java
+++
@@ -1,9 +0,0 @@
-package com.prodama.model;
-
-public enum TipoBase {
-	  Produção(),
-	  Homologação,
-	  Desenvolvimento,
-	  VPN,
-	  Outro;
-	}
